2fe4a8c12023b88c7803b21f5a83e6f8502a89b4
haoyingang25 <2005anghy@gmail.com>
2025-01-07 20:49:44 +0800

    changed font color and header for cuisines gallery

--- app/cuisine/page.jsx
+++ app/cuisine/page.jsx
@@ -3,15 +3,25 @@
 
 import React from "react";
 import Carousel from "../gallery/carousel"; // Correct relative path
- // Adjust path if necessary
 
 const CuisinePage = () => {
   return (
     <div>
-      <h1 style={{ textAlign: "center", marginTop: "20px" }}>Cuisine Page</h1>
+      <h1 
+        style={{
+          textAlign: "center",
+          marginTop: "20px",
+          color: "#B45F04", // Warm brown-orange font color
+          fontFamily: "'Lora', serif", // Elegant font
+          fontWeight: "bold",
+        }}
+      >
+        Visit the Different Cuisines!
+      </h1>
       <Carousel />
     </div>
   );
 };
 
 export default CuisinePage;
+
