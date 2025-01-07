7cc9c66caaf5d8a88e7dd6d6b424882b85964b02
haoyingang25 <2005anghy@gmail.com>
2025-01-07 14:53:31 +0800

    Added styles, carosel

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+// app/page.js
+import React from "react";
+import Carousel from "./gallery/carousel";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Cuisine Gallery</h1>
+      <Carousel />
+    </main>
+  );
+}
